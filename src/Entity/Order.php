00be6ad6b2eaf989a71021b50dea9c8424c7fbc5
RKhazaei <302769656@student.rocmondriaan.nl>
2023-03-14 12:54:17 +0100

    bestelform gemaakt db side still not done

---
+++ src/Entity/Order.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\OrderRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: OrderRepository::class)]
+#[ORM\Table(name: '`order`')]
+class Order
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $naam = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $adres = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nummer = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $email = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNaam(): ?string
+    {
+        return $this->naam;
+    }
+
+    public function setNaam(string $naam): self
+    {
+        $this->naam = $naam;
+
+        return $this;
+    }
+
+    public function getAdres(): ?string
+    {
+        return $this->adres;
+    }
+
+    public function setAdres(string $adres): self
+    {
+        $this->adres = $adres;
+
+        return $this;
+    }
+
+    public function getNummer(): ?string
+    {
+        return $this->nummer;
+    }
+
+    public function setNummer(string $nummer): self
+    {
+        $this->nummer = $nummer;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+}
