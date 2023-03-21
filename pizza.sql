2f3f495fe16792b359bbec8fdd8a17d024033228
RKhazaei <302769656@student.rocmondriaan.nl>
2023-03-21 10:31:36 +0100

    Update pizza.sql

--- pizza.sql
+++ pizza.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Gegenereerd op: 10 mrt 2023 om 10:25
+-- Gegenereerd op: 21 mrt 2023 om 10:28
 -- Serverversie: 10.4.24-MariaDB
 -- PHP-versie: 8.1.6
 
@@ -28,9 +28,9 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `category` (
-                            `id` int(11) NOT NULL,
-                            `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-                            `picture` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+  `id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `picture` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
@@ -38,9 +38,9 @@ CREATE TABLE `category` (
 --
 
 INSERT INTO `category` (`id`, `name`, `picture`) VALUES
-                                                     (1, 'vlees', 'pizza-vlees.jpg'),
-                                                     (2, 'veggies', 'pizza-veg.jpg'),
-                                                     (3, 'fish', 'pizza-fish.jpg');
+(1, 'vlees', 'pizza-vlees.jpg'),
+(2, 'veggies', 'pizza-veg.jpg'),
+(3, 'fish', 'pizza-fish.jpg');
 
 -- --------------------------------------------------------
 
@@ -49,9 +49,9 @@ INSERT INTO `category` (`id`, `name`, `picture`) VALUES
 --
 
 CREATE TABLE `doctrine_migration_versions` (
-                                               `version` varchar(191) COLLATE utf8_unicode_ci NOT NULL,
-                                               `executed_at` datetime DEFAULT NULL,
-                                               `execution_time` int(11) DEFAULT NULL
+  `version` varchar(191) COLLATE utf8_unicode_ci NOT NULL,
+  `executed_at` datetime DEFAULT NULL,
+  `execution_time` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
@@ -59,10 +59,12 @@ CREATE TABLE `doctrine_migration_versions` (
 --
 
 INSERT INTO `doctrine_migration_versions` (`version`, `executed_at`, `execution_time`) VALUES
-                                                                                           ('DoctrineMigrations\\Version20230216110449', '2023-03-08 09:01:01', 137),
-                                                                                           ('DoctrineMigrations\\Version20230223103345', '2023-03-08 09:01:01', 172),
-                                                                                           ('DoctrineMigrations\\Version20230308085021', '2023-03-08 09:50:37', 153),
-                                                                                           ('DoctrineMigrations\\Version20230308092409', '2023-03-08 10:24:23', 35);
+('DoctrineMigrations\\Version20230216110449', '2023-03-08 09:01:01', 137),
+('DoctrineMigrations\\Version20230223103345', '2023-03-08 09:01:01', 172),
+('DoctrineMigrations\\Version20230308085021', '2023-03-08 09:50:37', 153),
+('DoctrineMigrations\\Version20230308092409', '2023-03-08 10:24:23', 35),
+('DoctrineMigrations\\Version20230321091912', '2023-03-21 10:19:22', 64),
+('DoctrineMigrations\\Version20230321092724', '2023-03-21 10:27:30', 179);
 
 -- --------------------------------------------------------
 
@@ -71,13 +73,28 @@ INSERT INTO `doctrine_migration_versions` (`version`, `executed_at`, `execution_
 --
 
 CREATE TABLE `messenger_messages` (
-                                      `id` bigint(20) NOT NULL,
-                                      `body` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-                                      `headers` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-                                      `queue_name` varchar(190) COLLATE utf8mb4_unicode_ci NOT NULL,
-                                      `created_at` datetime NOT NULL,
-                                      `available_at` datetime NOT NULL,
-                                      `delivered_at` datetime DEFAULT NULL
+  `id` bigint(20) NOT NULL,
+  `body` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `headers` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue_name` varchar(190) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` datetime NOT NULL,
+  `available_at` datetime NOT NULL,
+  `delivered_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `order`
+--
+
+CREATE TABLE `order` (
+  `id` int(11) NOT NULL,
+  `naam` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `adres` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nummer` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `pizza_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -87,10 +104,10 @@ CREATE TABLE `messenger_messages` (
 --
 
 CREATE TABLE `pizza` (
-                         `id` int(11) NOT NULL,
-                         `category_id` int(11) NOT NULL,
-                         `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-                         `picture` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+  `id` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `picture` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
@@ -98,15 +115,15 @@ CREATE TABLE `pizza` (
 --
 
 INSERT INTO `pizza` (`id`, `category_id`, `name`, `picture`) VALUES
-                                                                 (1, 1, 'pepperoni', 'pepperoni.jpg'),
-                                                                 (2, 1, 'pizza hawai', 'pizza-hawai.jpg'),
-                                                                 (3, 1, 'pizza americana', 'pizza-americano.jpg'),
-                                                                 (4, 3, 'salmon pizza', 'pizza-salmon.jpg'),
-                                                                 (5, 3, 'shrimp pizza', 'pizza-shrimp.jpg'),
-                                                                 (6, 3, 'pizza harring', 'pizza-harring.jpg'),
-                                                                 (7, 2, 'pizza funghi', 'pizza-funghi.jpg'),
-                                                                 (8, 2, 'pizza paprika', 'pizza-paprika.jpg'),
-                                                                 (10, 2, 'pizza olives', 'pizza-olives.jpg');
+(1, 1, 'pepperoni', 'pepperoni.jpg'),
+(2, 1, 'pizza hawai', 'pizza-hawai.jpg'),
+(3, 1, 'pizza americana', 'pizza-americano.jpg'),
+(4, 3, 'salmon pizza', 'pizza-salmon.jpg'),
+(5, 3, 'shrimp pizza', 'pizza-shrimp.jpg'),
+(6, 3, 'pizza harring', 'pizza-harring.jpg'),
+(7, 2, 'pizza funghi', 'pizza-funghi.jpg'),
+(8, 2, 'pizza paprika', 'pizza-paprika.jpg'),
+(10, 2, 'pizza olives', 'pizza-olives.jpg');
 
 -- --------------------------------------------------------
 
@@ -115,10 +132,10 @@ INSERT INTO `pizza` (`id`, `category_id`, `name`, `picture`) VALUES
 --
 
 CREATE TABLE `product` (
-                           `id` int(11) NOT NULL,
-                           `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-                           `price` int(11) NOT NULL,
-                           `category_id` int(11) NOT NULL
+  `id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `price` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
@@ -129,35 +146,42 @@ CREATE TABLE `product` (
 -- Indexen voor tabel `category`
 --
 ALTER TABLE `category`
-    ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexen voor tabel `doctrine_migration_versions`
 --
 ALTER TABLE `doctrine_migration_versions`
-    ADD PRIMARY KEY (`version`);
+  ADD PRIMARY KEY (`version`);
 
 --
 -- Indexen voor tabel `messenger_messages`
 --
 ALTER TABLE `messenger_messages`
-    ADD PRIMARY KEY (`id`),
+  ADD PRIMARY KEY (`id`),
   ADD KEY `IDX_75EA56E0FB7336F0` (`queue_name`),
   ADD KEY `IDX_75EA56E0E3BD61CE` (`available_at`),
   ADD KEY `IDX_75EA56E016BA31DB` (`delivered_at`);
 
 --
+-- Indexen voor tabel `order`
+--
+ALTER TABLE `order`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_F5299398D41D1D42` (`pizza_id`);
+
+--
 -- Indexen voor tabel `pizza`
 --
 ALTER TABLE `pizza`
-    ADD PRIMARY KEY (`id`),
+  ADD PRIMARY KEY (`id`),
   ADD KEY `IDX_CFDD826F12469DE2` (`category_id`);
 
 --
 -- Indexen voor tabel `product`
 --
 ALTER TABLE `product`
-    ADD PRIMARY KEY (`id`),
+  ADD PRIMARY KEY (`id`),
   ADD KEY `IDX_D34A04AD12469DE2` (`category_id`);
 
 --
@@ -168,41 +192,53 @@ ALTER TABLE `product`
 -- AUTO_INCREMENT voor een tabel `category`
 --
 ALTER TABLE `category`
-    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT voor een tabel `messenger_messages`
 --
 ALTER TABLE `messenger_messages`
-    MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT voor een tabel `order`
+--
+ALTER TABLE `order`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT voor een tabel `pizza`
 --
 ALTER TABLE `pizza`
-    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT voor een tabel `product`
 --
 ALTER TABLE `product`
-    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Beperkingen voor geëxporteerde tabellen
 --
 
 --
+-- Beperkingen voor tabel `order`
+--
+ALTER TABLE `order`
+  ADD CONSTRAINT `FK_F5299398D41D1D42` FOREIGN KEY (`pizza_id`) REFERENCES `pizza` (`id`);
+
+--
 -- Beperkingen voor tabel `pizza`
 --
 ALTER TABLE `pizza`
-    ADD CONSTRAINT `FK_CFDD826F12469DE2` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`);
+  ADD CONSTRAINT `FK_CFDD826F12469DE2` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`);
 
 --
 -- Beperkingen voor tabel `product`
 --
 ALTER TABLE `product`
-    ADD CONSTRAINT `FK_D34A04AD12469DE2` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`);
+  ADD CONSTRAINT `FK_D34A04AD12469DE2` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
